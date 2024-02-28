f856a714786ff3b572681780074db0d67779783b
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2024-02-28 01:40:17 +0000

    Task 8.2 server.js

---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,15 @@
+// Task - 8: Organize a complex HTTP server using Express
+const express = require('express');
+
+const router = require('./routes/index');
+
+const app = express();
+const port = 1245;
+
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
+
+app.listen(port);
+
+export default app;
