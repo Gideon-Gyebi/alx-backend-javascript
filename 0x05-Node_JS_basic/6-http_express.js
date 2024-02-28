38ca3e50cd00730b2fd937e2d65289c10a6f1c03
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2024-02-28 00:51:38 +0000

    Task 6-http_express.js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+// Task - 6: Create a small HTTP server using Express
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+});
+
+module.exports = app;
