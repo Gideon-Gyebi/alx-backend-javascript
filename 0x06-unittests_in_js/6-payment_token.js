eb67af0687169da5dd519da38d6c4011c8133bbd
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2024-03-02 00:58:19 +0000

    Task 6-payment_token.js

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,10 @@
+// Task ~ 6
+function getPaymentTokenFromAPI(success) {
+    if (success) {
+	return new Promise((resolve) => {
+          resolve({data: 'Successful response from the API' });
+	});
+    }
+}
+
+module.exports = getPaymentTokenFromAPI;
